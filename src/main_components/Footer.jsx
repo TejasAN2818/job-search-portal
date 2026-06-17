a70f0a53bddd942f5a24e1c1a985ccb2c4221ffd
tejas2818 <tejastejas2818@gmail.com>
2026-06-18 01:53:15 +0530

    ready for apply monitization 2

--- src/main_components/Footer.jsx
+++ src/main_components/Footer.jsx
@@ -9,13 +9,13 @@ export default function Footer() {
     mt-6
     py-2
     px-2
-    pb-10
+    pb-6
   "
     >
 
       <div className="max-w-4xl mx-auto text-center">
 
-        <div className="text-[10px] leading-tight">
+        <div className="text-[12px] leading-tight">
 
           <p>
             -:👨‍💻 Developer Details:-
@@ -28,10 +28,13 @@ export default function Footer() {
           <p>
             📧 tejastejas2818@gmail.com
           </p>
+          <p>
+
+          </p>
 
         </div>
 
-        <p className="mt-1 text-[10px] text-gray-500">
+        <p className="mt-1 text-[12px] text-gray-500">
           Version 1.0 © 2026 Work Station. All Rights Reserved.
         </p>
 
